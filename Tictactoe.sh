2d61c1e11d9f5a11fe38f228228213b4b23d12e7
amritha-tk <amrithatk95@gmail.com>
2020-07-08 23:52:24 +0530

    [amritha]Added UC1 for resettingboard

--- Tictactoe.sh
+++ Tictactoe.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "WELCOME"
+resetBoard(){
+  echo "====================="
+  Arr=(. . . . . . . . .)
+  player=1
+  gamestatus=1
+  echo "====================="
+  echo "Game started"
+}
+
+resetBoard
