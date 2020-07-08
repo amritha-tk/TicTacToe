9a6e0939b8d3410a97ee45809363e614a07c61db
amritha-tk <amrithatk95@gmail.com>
2020-07-09 00:02:27 +0530

    [amritha]Added UC2 for the toss to decide which player to play

--- Tictactoe.sh
+++ Tictactoe.sh
@@ -4,10 +4,19 @@ echo "WELCOME"
 resetBoard(){
   echo "====================="
   Arr=(. . . . . . . . .)
-  player=1
-  gamestatus=1
+  PLAYER=1
+  GAME_STATUS=1
   echo "====================="
   echo "Game started"
 }
 
-resetBoard
+toss(){
+  player=$((RANDOM%2))
+  if [ $player -eq '1' ]
+    then
+      echo "Player 1's Turn"
+    else
+      echo "Player 2's Turn"
+  fi
+}
+toss
