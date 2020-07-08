3b3e0a4b188ac2f88be30e554e93e169fc90a59e
amritha-tk <amrithatk95@gmail.com>
2020-07-09 00:34:58 +0530

    [amritha]Added the table view

--- Tictactoe.sh
+++ Tictactoe.sh
@@ -4,8 +4,6 @@ echo "WELCOME"
 resetBoard(){
   echo "====================="
   Arr=(. . . . . . . . .)
-  PLAYER=1
-  GAME_STATUS=1
   echo "====================="
   echo "Game started"
 }
@@ -20,4 +18,12 @@ toss(){
   fi
 }
 toss
+
+viewBoard(){
+  echo "r\c 0 1 2"
+  echo "0   ${Arr[0]} ${Arr[1]} ${Arr[2]}"
+  echo "1   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
+  echo "2   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
+}
+viewBoard
 
