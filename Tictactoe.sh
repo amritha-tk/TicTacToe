202083d9a05f20977b58ee069be8b487b6453aa7
amritha-tk <amrithatk95@gmail.com>
2020-07-09 22:36:23 +0530

    [amritha]Added App logic

--- Tictactoe.sh
+++ Tictactoe.sh
@@ -4,26 +4,29 @@ echo "WELCOME"
 resetBoard(){
   echo "====================="
   Arr=(. . . . . . . . .)
+  gamestatus=1
   echo "====================="
   echo "Game started"
 }
-resetBoard
+#resetBoard
 
 toss(){
- player=$((RANDOM%2))
+ player=$((RANDOM%2+1))
   if [ $player -eq '1' ]
     then
       echo "I have won the toss and Starting the game "
-	  read -p "What symbol do you choose 1.X  2.O" sym
+	  read -p "What symbol do you choose 1.X  2.O" sym_num
     else
       echo "Computer won the game and starting the game"
-	  sym=$((RANDOM%2+1))
-  fi
+	  sym_num=$((RANDOM%2+1))
+
+  fi 
+
 }
 
 choose(){
 toss
-if [ $sym -eq '1' ]
+if [ $sym_num -eq '1' ]
 	then
 		sym="X"
 		echo "Chooses X"
@@ -31,8 +34,9 @@ if [ $sym -eq '1' ]
 		sym="O"
 		echo "Chooses O"
 fi
+
 }
-choose
+
 
 viewBoard(){
   echo "r\c 0 1 2"
@@ -40,7 +44,94 @@ viewBoard(){
   echo "1   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
   echo "2   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
 }
-viewBoard
 
 
+checkmatch(){
+  if [ ${Arr[$1]} != "." ] && [ ${Arr[$1]} == ${Arr[$2]} ] && [ ${Arr[$2]} == ${Arr[$3]} ]; then
+    gamestatus=0
+  fi
+}
+
+checkgame(){
+  checkmatch 0 1 2
+  checkmatch 3 4 5
+  checkmatch 6 7 8
+  checkmatch 0 3 6
+  checkmatch 1 4 7
+  checkmatch 2 5 8
+  checkmatch 0 4 8
+  checkmatch 2 4 6
+}
+
+set(){
+  idx=$(( $1 * 3 + $2 ))
+  if [ ${Arr[$idx]} == "." ]; then
+    Arr[$idx]=$3
+    player=$((player%2+1))
+
+    sym_num=$((sym_num%2+1))
+    case  $sym_num in
+    1)
+      sym="X"
+      ;;
+    2)
+      sym="O"
+      ;;
+    *)
+      echo "Unknown symbol"
+      ;;
+    esac
+     
+    else
+    echo "You can't place there!"
+  fi
+}
+
+# Main program starts here
+
+resetBoard
+choose
+
+ # infinite game loop
+while [ 1 == 1 ]; do
+  echo ""
+  echo "Player $player's turn: ($sym)"
+
+  viewBoard
+  echo ""
+  echo "  Command:"
+  echo "        1. set [row] [column]"
+  echo "        2. restart"
+  
+# For tic tac game logic
+    while [ 1 == 1 ]; do
+    read -r cmd a b
+    if [ $cmd == "set" ]; then
+        set $a $b $sym $sym_num
+        break
+    elif [ $cmd == "restart" ]; then
+        resetBoard
+        break
+    else
+        echo "wrong command, try again."
+    fi
+  done
+
+  checkgame
+
+# To check the status of the game and to move forward  
+    if [ $gamestatus == 0 ]; then
+    echo "Gameover"
+    player=$((player%2+1))
+    echo "Player $player ($sym) win!!"
+    echo "Press restart to reset the board"
+    while [ 1 == 1 ]; do
+        read -r cmd n
+        if [ $cmd == "restart" ]; then
+          resetBoard
+          break
+        fi
+    done
+  fi
+done
 
